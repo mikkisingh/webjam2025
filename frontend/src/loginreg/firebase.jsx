4de4b1114a8339a61ed097f81ee1953d35ca10b6
Dominic Hubschmitt <dhubschm@uci.edu>
2025-11-07 12:56:23 -0800

    Login page should work, there are two text files named Appjsx_as_txt and indexcss_as_txt that contain edits to files you may have. The other files are new and added and should not affect overall functionality

---
+++ frontend/src/loginreg/firebase.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { getAuth } from "firebase/auth"; 
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3nn1XGtwPhqnaNPtqoe6Du8b_eFdYvY8",
+  authDomain: "medicheck-71418.firebaseapp.com",
+  projectId: "medicheck-71418",
+  storageBucket: "medicheck-71418.firebasestorage.app",
+  messagingSenderId: "1016966698695",
+  appId: "1:1016966698695:web:533fe58e680ce19e9c50e2"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
